660344453fd2472aec64a6f751c7d60028a52710
David Molineus <mail@netzmacht.de>
2014-07-22 19:06:01 +0200

    add languages for button elements

--- module/languages/de/default.php
+++ module/languages/de/default.php
@@ -6,6 +6,10 @@
 $GLOBALS['TL_LANG']['CTE']['bootstrap_carousel'] = 'Carousel-Element';
 $GLOBALS['TL_LANG']['MSC']['bootstrap_carousel'] = 'Bootstrap Carousel: %s';
 $GLOBALS['TL_LANG']['CTE']['bootstrap_tabs']     = 'Reiter-Elemente';
+$GLOBALS['TL_LANG']['CTE']['bootstrap_button'][0] = 'Schaltfläche';
+$GLOBALS['TL_LANG']['CTE']['bootstrap_button'][1] = 'Stellt eine Schaltfläche als Inhaltselement zur Verfügung.';
+$GLOBALS['TL_LANG']['CTE']['bootstrap_buttons'][0] = 'Kombinierte Schaltfächen ';
+$GLOBALS['TL_LANG']['CTE']['bootstrap_buttons'][1] = 'Element für kombinierte Schaltflächen wie Toolbars/Gruppes/Dropdowns';
 
 /*
  * content elements
--- module/languages/en/default.php
+++ module/languages/en/default.php
@@ -6,6 +6,10 @@
 $GLOBALS['TL_LANG']['CTE']['bootstrap_carousel'] = 'Carousel elements';
 $GLOBALS['TL_LANG']['MSC']['bootstrap_carousel'] = 'Bootstrap Carousel: %s';
 $GLOBALS['TL_LANG']['CTE']['bootstrap_tabs']     = 'Tab elements';
+$GLOBALS['TL_LANG']['CTE']['bootstrap_button'][0] = 'Button';
+$GLOBALS['TL_LANG']['CTE']['bootstrap_button'][1] = 'Allocates a button as a content element.';
+$GLOBALS['TL_LANG']['CTE']['bootstrap_buttons'][0] = 'Combined buttons ';
+$GLOBALS['TL_LANG']['CTE']['bootstrap_buttons'][1] = 'Element for combined buttons like toolbars/groups/dropdowns.';
 
 /*
  * content elements
